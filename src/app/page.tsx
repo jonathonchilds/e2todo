66b9c9de28ceea40e4f35450f20da28b31bd4590
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-07-21 20:49:59 -0400

    adds react-beautiful-dnd library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import TodoList from "../components/TodoList";
 import TodoInputField from "../components/TodoInputField";
 import PageHeader from "../components/PageHeader";
 
+//The role of onDragEnd is to synchronously update the state of our app to reflect the drag and drop result.
+
 export default function Page() {
   return (
     <main className="flex flex-col desktop:w-5/12 w-80 mx-auto relative ">
