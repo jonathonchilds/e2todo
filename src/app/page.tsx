80f12c08bf34c1c6130d00a4462571bedc2fcfad
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2023-07-21 20:54:36 -0400

    adds DragDropContext to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,23 @@ import TodoList from "../components/TodoList";
 import TodoInputField from "../components/TodoInputField";
 import PageHeader from "../components/PageHeader";
 
+import {
+  DragDropContext,
+  DropResult,
+  OnDragEndResponder,
+} from "react-beautiful-dnd";
+
 //The role of onDragEnd is to synchronously update the state of our app to reflect the drag and drop result.
+const onDragEnd = (result: DropResult) => {};
 
 export default function Page() {
   return (
     <main className="flex flex-col desktop:w-5/12 w-80 mx-auto relative ">
       <PageHeader />
       <TodoInputField />
-      <TodoList />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <TodoList />
+      </DragDropContext>
       <Footer />
     </main>
   );
